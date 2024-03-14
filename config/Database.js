31f9df74df5c01667697a1286afb5163dc33506f
Samuel <samnmbuguah@gmail.com>
2024-03-14 18:28:56 +0300

    🗃️ Update database password in config file

--- config/Database.js
+++ config/Database.js
@@ -1,6 +1,17 @@
 const  {Sequelize} = require("sequelize");
 
-const db = new Sequelize('dualnet_db', 'dualnetadmin', 'password', {
+const db = new Sequelize('dualnet_db', 'dualnetadmin', 'arbitrage', {
     host: "localhost",
     dialect: "mysql"
 });
+
+module.exports = db;
+
+
+
+// const db = new Sequelize('dualnet_db','root','password',{  //'dualnet_db','fasqazjz_manager','kkk123!@#W',{ 
+//     host: "localhost",
+//     dialect: "mysql"
+// });
+
+// module.exports = db;
