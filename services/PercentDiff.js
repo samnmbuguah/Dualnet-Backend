91ba1cc1982008f28a3ab66baea8a19fbd8fea97
Samuel <samnmbuguah@gmail.com>
2024-03-15 18:41:54 +0300

    ✨ Add PercentDiff service file

---
+++ services/PercentDiff.js
@@ -0,0 +1,29 @@
+const GateWebSocket = require('./StreamPrices.js');
+const MatchingPairs = require('../models/MatchingPairsModel.js');
+require('dotenv').config();
+
+const API_KEY = process.env.API_KEY;
+const API_SECRET = process.env.API_SECRET;
+const wsFuturesUrl = 'wss://fx-ws.gateio.ws/v4/ws/usdt'
+const wsSpotUrl = 'wss://api.gateio.ws/ws/v4/'
+
+let tickers = [];
+
+MatchingPairs.findAll({
+    limit: 5
+}).then(records => {
+    tickers = records.map(record => record.id);
+    const ws = new GateWebSocket([wsFuturesUrl, wsSpotUrl], API_KEY, API_SECRET, tickers);
+});
+
+process.on('SIGINT', function () {
+    console.log("Caught interrupt signal, closing websockets");
+
+    // Close all WebSocket connections
+    for (let websocket of ws.webSockets) {
+        websocket.close();
+    }
+
+    // Exit the process
+    process.exit();
+});
