37821b92de373baab9b684e9dc4131156eaf7e52
Samuel <samnmbuguah@gmail.com>
2024-03-15 17:27:17 +0300

    ✨ Refactor PopulateTables.js to include MatchingPairsModel.js

--- config/PopulateTables.js
+++ config/PopulateTables.js
@@ -1,16 +1,21 @@
 const Contracts = require('../models/FuturesModel.js');
 const Currencies = require('../models/SpotModel.js');
+const MatchingPairs = require('../models/MatchingPairsModel.js');
 const {fetchSpotPairs, fetchFuturesContracts, findMatchingPairs} = require( '../Services/GateioServices.js');
 async function populateTables() {
     try {
-        const spotPairs = await fetchSpotPairs();
-        const futuresContracts = await fetchFuturesContracts();
+        // const spotPairs = await fetchSpotPairs();
+        // const futuresContracts = await fetchFuturesContracts();
 
-        // Assuming spotPairs and futuresContracts are arrays of objects
-        // where each object's keys match the fields of your models
+        // // Assuming spotPairs and futuresContracts are arrays of objects
+        // // where each object's keys match the fields of your models
 
-        await Currencies.bulkCreate(spotPairs);
-        await Contracts.bulkCreate(futuresContracts);
+        // await Currencies.bulkCreate(spotPairs);
+        // await Contracts.bulkCreate(futuresContracts);
+
+        // Find matching pairs
+        const matchingPairs = await findMatchingPairs();
+        await MatchingPairs.bulkCreate(matchingPairs);
 
         console.log('Tables have been populated');
     } catch (error) {
