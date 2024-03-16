7fbb3f66c2df107f3ba442d8bcbe343f9fe53996
Samuel <samnmbuguah@gmail.com>
2024-03-16 15:51:20 +0300

    ✅

---
+++ services/TestSocket.js
@@ -0,0 +1,16 @@
+// const WebSocket = require('ws');
+
+// const ws = new WebSocket('ws://localhost:3001'); 
+
+// ws.on('open', function open() {
+//     console.log('connected');
+//     ws.send(Date.now());
+// });
+
+// ws.on('close', function close() {
+//     console.log('disconnected');
+// });
+
+// ws.on('message', function incoming(data) {
+//     console.log(`Roundtrip time: ${Date.now() - data} ms`);
+// });
