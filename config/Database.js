7c4a1108f75248aec0def47a324e031970796622
Samuel <samnmbuguah@gmail.com>
2024-03-16 13:38:48 +0300

    🗃️ Update database configuration

--- config/Database.js
+++ config/Database.js
@@ -1,14 +1,14 @@
 const  {Sequelize} = require("sequelize");
 
 
-const db = new Sequelize('dualnet_db', 'dualnetadmin', 'arbitrage', {
-    host: "db",
-    dialect: "mysql"
-});
-
 // const db = new Sequelize('dualnet_db', 'dualnetadmin', 'arbitrage', {
-//     host: "localhost",
+//     host: "db",
 //     dialect: "mysql"
 // });
+
+const db = new Sequelize('dualnet_db', 'dualnetadmin', 'arbitrage', {
+    host: "localhost",
+    dialect: "mysql"
+});
 
 module.exports = db;
