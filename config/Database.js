ed2e8b63b369e1722fc35c2b3daff3659e2ff47a
Samuel <samnmbuguah@gmail.com>
2024-03-17 09:12:54 +0300

    🔥 Remove Dockerfile, devcontainer.json, and docker-compose.yml

--- config/Database.js
+++ config/Database.js
@@ -1,11 +1,5 @@
 const  {Sequelize} = require("sequelize");
 
-
-// const db = new Sequelize('dualnet_db', 'dualnetadmin', 'arbitrage', {
-//     host: "db",
-//     dialect: "mysql"
-// });
-
 const db = new Sequelize('dualnet_db', 'dualnetadmin', 'arbitrage', {
     host: "localhost",
     dialect: "mysql"
