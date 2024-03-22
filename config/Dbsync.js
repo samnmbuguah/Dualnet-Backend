1c0228e8f35b2ac60dd41930deffbf3bcdd33d38
Samuel <samnmbuguah@gmail.com>
2024-03-22 19:21:07 +0300

    🗃️ Update database synchronization options

--- config/Dbsync.js
+++ config/Dbsync.js
@@ -1,12 +1,10 @@
 const db = require('./Database.js'); 
 const Users = require('../models/UserModel.js');
-const Contracts = require('../models/FuturesModel.js');
-const Currencies = require('../models/SpotModel.js');
 const MatchingPairs = require('../models/MatchingPairsModel.js');
 const UserPDFs = require('../models/UserPDFsModel.js');
 const Scans = require('../models/ScansModel.js');
 
-db.sync()
+db.sync({ force: true }, { alter: true })
     .then(() => console.log('Tables have been created'))
     .catch(error => console.log('Error occurred:', error));
 
