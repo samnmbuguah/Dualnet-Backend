e67e984d3dae9784f93e451e0c9d7e12c90f4099
Samuel <samnmbuguah@gmail.com>
2024-03-23 01:48:16 +0300

    ♻️ Refactor CORS configuration and remove unused code

--- index.js
+++ index.js
@@ -1,44 +1,26 @@
-const db = require("./config/Database.js");
-require('./config/Dbsync.js');
+require('dotenv').config();
 const express = require('express');
 const path = require("path");
 const app = express();
+const cors = require('cors');
 const server = require('http').createServer(app); // Create server with Express app
 const cron = require('node-cron');
+// app.use(cors());
+app.use(cors({
+  origin: 'https://dualnet-production.up.railway.app' //client's origin
+}));
 const populateTables = require('./jobs/PopulateTables.js');
-const cors = require('cors');
-const io = require('socket.io')(server,{ // Pass server to Socket.IO
-    cors: {
-        origin: "*",
-    }
-});
-const dotenv = require("dotenv");
-const router = require("./routes/index.js");
-
-dotenv.config();
-const PORT =process.env.PORT || 3042;
-const { fileURLToPath } = require('url');
-const { getAccountsByUserid } = require('./controllers/Users.js')
-const {init, getAccountInfo, getAgentInfo, getAccountIdsByPageNum } = require('./controllers/MTAPI.js'); 
+const StreamPrices = require('./services/StreamPrices.js');
+const router = require("./routes/Routes.js");
+const PORT =process.env.PORT || 3042; 
 const generatePdfCronJob = require('./jobs/generatePDF.js');
-
-try {
-    db.authenticate();
-} catch (error) {
-    console.error(error);
-}
-
-(async () => {
-    await populateTables();
-})();
 
 app.use(express.json());
 app.use('/api', router);
 app.use('/pdfs', express.static(path.join(__dirname, '..', 'pdfs')));
 generatePdfCronJob();
 
-require('./services/StreamPrices.js')(io);
-
+StreamPrices(server)
 
 // Serve static files from the React frontend app
 app.use(express.static(path.join(__dirname, '../FrontendDualnet/build')))
@@ -50,52 +32,3 @@ app.get('*', (req, res) => {
 
 cron.schedule('0 * * * *', populateTables);
 server.listen(PORT, ()=> console.log(`Server running at port ${PORT}`)); // Start server 
-
-
-
-
-// //realtime send data via socket.io
-// io.on('connect', (client) => {
-// 	let myInterval1;
-//     let myInterval2;
-//     let isloading1 = 0;
-//     let isloading2 = 0;
-
-//     client.on('oneInfo', async(userid, currency='') => {
-
-//         clearInterval(myInterval1);
-
-//         let accInfo = await getAccountsByUserid(userid);
-//         myInterval1 = setInterval(async() => {
-//             if(isloading1 == 0){
-//                 isloading1 = 1;
-//                 const arr_info = await getAccountInfo(accInfo, currency);
-                
-//                 isloading1 = 0;
-//                 if(arr_info.length > 0) {
-//                     client.emit('update-information', JSON.stringify(arr_info));
-//                 }
-//             }
-//         }, 1500);
-//     });
-
-//     client.on('allInfo', async(page, currency='') => {
-        
-//         clearInterval(myInterval2);
-
-//         let accs_obj = getAccountIdsByPageNum(page);
-//         myInterval2 = setInterval(async() => {
-//             if(isloading2 == 0){
-//                 isloading2 = 1;
-//                 const agent_info = await getAgentInfo(accs_obj);
-//                 isloading2 = 0;
-//                 client.emit('update-users-information', JSON.stringify(agent_info));
-//             }
-//         }, 1500);
-//     });
-
-//     client.on('disconnect', () => {
-//         clearInterval(myInterval1);
-//         clearInterval(myInterval2);
-//     });
-// });
