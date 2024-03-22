5abe4a080a251cf413c7e0fcc69d81c7bf973547
Samuel <samnmbuguah@gmail.com>
2024-03-22 19:17:01 +0300

    🚚 Rename TestDbConn.js for database connection testing

--- config/TestDbConn.js
+++ config/TestDbConn.js
@@ -1,6 +1,5 @@
 const db = require('./Database.js'); 
 
-
 db.authenticate()
-    .then(() => console.log('Database connection has been established successfully.'))
+    .then(() => console.log('Connection has been established successfully.'))
     .catch(error => console.error('Unable to connect to the database:', error));
