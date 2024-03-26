e3f347d4b6d599e83ac2459a1fa15c911fb5c800
Samuel <samnmbuguah@gmail.com>
2024-03-26 18:47:55 +0300

    🗃️ Drop Scans table before syncing database

--- config/Dbsync.js
+++ config/Dbsync.js
@@ -4,6 +4,10 @@ const MatchingPairs = require('../models/MatchingPairsModel.js');
 const UserPDFs = require('../models/UserPDFsModel.js');
 const Scans = require('../models/ScansModel.js');
 
-db.sync()
+Scans.drop()
+    .then(() => {
+        console.log('Scans table has been dropped');
+        return db.sync();
+    })
     .then(() => console.log('Tables have been created'))
     .catch(error => console.log('Error occurred:', error));
