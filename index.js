ea370a9b80f06a6b0ccd8d5f95514f2bdf6333bf
Samuel <samnmbuguah@gmail.com>
2024-03-27 22:05:55 +0300

    ♻️ Refactor CORS configuration and add error handler

--- index.js
+++ index.js
@@ -1,34 +1,48 @@
 require('dotenv').config();
 const express = require('express');
-const path = require("path");
 const app = express();
+const path = require("path");
 const cors = require('cors');
-const server = require('http').createServer(app); // Create server with Express app
 const cron = require('node-cron');
-// app.use(cors());
-app.use(cors({
-    origin: ['https://dualnet-production.up.railway.app', 'http://localhost:3042', 'http://localhost:3000', 'http://dualnet.railway.internal'],
-  }));
+const server = require('http').createServer(app); // Create server with Express app
+
+// Check for required environment variables
+if (!process.env.PORT) {
+  console.error("Missing PORT environment variable. Please check your .env file");
+  process.exit(1);
+}
+
+// CORS configuration
+const corsOptions = {
+  origin: ['https://dualnet-production.up.railway.app', 'http://localhost:3042', 'http://localhost:3000', 'http://dualnet.railway.internal'],
+};
+app.use(cors(corsOptions));
+
 const populateTables = require('./jobs/PopulateTables.js');
 const StreamPrices = require('./services/StreamPrices.js');
 const router = require("./routes/Routes.js");
 const PORT = process.env.PORT || 3042; 
-const generatePdfCronJob = require('./jobs/generatePDF.js');
 
 app.use(express.json());
 app.use('/api', router);
-app.use('/pdfs', express.static(path.join(__dirname, '..', 'pdfs')));
-generatePdfCronJob();
-
-StreamPrices(server)
 
 // Serve static files from the React frontend app
 app.use(express.static(path.join(__dirname, '../FrontendDualnet/build')))
 
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../FrontendDualnet/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../FrontendDualnet/build', 'index.html'));
+});
+
+// Global error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at port ${PORT}`);
+  StreamPrices(server); // Start streaming prices after the server has started
 });
 
 cron.schedule('0 * * * *', populateTables);
-server.listen(PORT, ()=> console.log(`Server running at port ${PORT}`)); // Start server 
