0e58e4027e1eaedaf80bc437766d2fec6e322173
Samuel <samnmbuguah@gmail.com>
2024-03-27 22:14:06 +0300

    🐛 Refactor TestPolling.js to use http and socket.io-client

--- services/TestPolling.js
+++ services/TestPolling.js
@@ -1,17 +1,51 @@
-const WebSocket = require('ws');
+const http = require('http');
+const StreamPrices = require('./StreamPrices');
+const io = require('socket.io-client');
 
-const ws = new WebSocket('ws://localhost:3042');
+let chai;
+let expect;
 
-ws.on('open', function open() {
-    console.log('connected');
-    ws.send(Date.now().toString()); // send the current timestamp to the server
-});
+describe('StreamPrices', function() {
+    this.timeout(5000); // Set timeout to 5 seconds for all hooks and tests in this suite
 
-ws.on('close', function close() {
-    console.log('disconnected');
-});
+    let server;
+    let clientSocket;
 
-ws.on('message', function incoming(data) {
-    const roundTripTime = Date.now() - parseInt(data);
-    console.log(`Round-trip time: ${roundTripTime} ms`);
+    before(async () => {
+        chai = await import('chai');
+        expect = chai.expect;
+    });
+
+    beforeEach((done) => {
+        // Setup
+        server = http.createServer().listen(3042);
+        StreamPrices(server);
+        clientSocket = io.connect('http://localhost:3042');
+        clientSocket.on('connect', done);
+    });
+
+    afterEach((done) => {
+        // Cleanup
+        if(clientSocket.connected) {
+            clientSocket.disconnect();
+        }
+        server.close(done);
+    });
+
+
+    it('should emit topScans', (done) => {
+        clientSocket.on('topScans', (topScans) => {
+            expect(topScans).to.be.an('array');
+            done();
+        });
+    });
+
+    it('should update scans on updateScans event', (done) => {
+        clientSocket.on('topScans', (topScans) => {
+            expect(topScans).to.be.an('array');
+            done();
+        });
+
+        clientSocket.emit('updateScans');
+    });
 });
