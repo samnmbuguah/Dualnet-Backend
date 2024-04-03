d985afc81ae6320ffbb1e338921aad6bed0a2c86
Samuel <samnmbuguah@gmail.com>
2024-04-04 00:37:21 +0300

    ♻️ Refactor setLeverage function to accept request and response objects

--- services/setLeverage.js
+++ services/setLeverage.js
@@ -3,20 +3,23 @@ const client = require('./gateClient');
 
 const api = new GateApi.FuturesApi(client);
 
-function setLeverage(settle, contract, leverage = "1") {
+function setLeverage(req, res) {
+    const { settle= 'usdt', contract, leverage = "1" } = req.body;
+
     const opts = {
         'crossLeverageLimit': "1" // string | Cross margin leverage(valid only when `leverage` is 0)
     };
 
-    return api.updatePositionLeverage(settle, contract, leverage, opts)
+    api.updatePositionLeverage(settle, contract, leverage, opts)
         .then(response => {
             console.log('Leverage changed successfully', response.body);
-            return response.body;
+            res.json(response.body);
         })
-        .catch(error => console.error(error.response.data));
+        .catch(error => {
+            console.error(error.response.data);
+            res.status(500).json({ error: error.response.data });
+        });
 }
 
 module.exports = setLeverage;
 
-// Call the function
-setLeverage('usdt', 'MOVEZ_USDT', "0")
