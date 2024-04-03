5e49ee0232669cfa42726a4384933f2b95330f0f
Samuel <samnmbuguah@gmail.com>
2024-04-03 07:52:26 +0300

    ✨ Add getPosition function to fetch futures position

---
+++ services/getPosition.js
@@ -0,0 +1,19 @@
+const GateApi = require('gate-api');
+const client = require('./gateClient');
+
+const api = new GateApi.FuturesApi(client);
+
+function fetchPosition(settle, contract) {
+    return api.getPosition(settle, contract)
+        .then(response => {
+            console.log('Futures position fetched. Size', response.body.size);
+            return response.body;
+        })
+        .catch(error => console.error(error.response.data));
+}
+
+module.exports = fetchPosition;
+
+// // Call the function
+// fetchFuturesPosition('usdt', 'MOVEZ_USDT')
+//     .then(() => console.log('Futures position fetched successfully'))
