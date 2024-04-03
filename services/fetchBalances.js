d4c26dcee80dc45228ab8c383964fe03760bb55b
Samuel <samnmbuguah@gmail.com>
2024-04-03 07:53:25 +0300

    ✨ Add fetchBalances.js to fetch spot and futures account balances

---
+++ services/fetchBalances.js
@@ -0,0 +1,42 @@
+const GateApi = require('gate-api');
+const client = require('./gateClient');
+
+// Fetch spot account balances
+function fetchSpotBalances() {
+    console.log('Fetching spot account balances...');
+    const spotApi = new GateApi.SpotApi(client);
+
+    return spotApi.listSpotAccounts()
+        .then(response => {
+            console.log('Fetched spot account balances');
+            const usdtBalance = response.body.find(account => account.currency === 'USDT');
+            console.log('Spot USDT balance: ', usdtBalance.available);
+            return response.body;
+        })
+        .catch(error => console.error(error));
+}
+
+// Fetch futures account balances
+function fetchFuturesBalances() {
+    console.log('Fetching futures account balances...');
+    const futuresApi = new GateApi.FuturesApi(client);
+
+    return futuresApi.listFuturesAccounts('usdt') // replace 'usdt' with your settle currency
+        .then(response => {
+            console.log('Fetched futures account balances');
+            console.log('Futures account balances: ', response.body.available);
+            return response.body;
+        })
+        .catch(error => console.error(error));
+}
+
+module.exports = {
+    fetchSpotBalances,
+    fetchFuturesBalances,
+};
+
+
+
+// Call the functions
+fetchSpotBalances();
+fetchFuturesBalances();
