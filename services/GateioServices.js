11a9feaada6ccd6198017d8508cc349244bc8ed0
Samuel <samnmbuguah@gmail.com>
2024-04-03 07:51:25 +0300

    ⚡️ Refactor GateioServices.js to use a separate gateClient module

--- services/GateioServices.js
+++ services/GateioServices.js
@@ -1,10 +1,5 @@
-require('dotenv').config();
 const GateApi = require('gate-api');
-const client = new GateApi.ApiClient();
-const API_KEY= process.env.API_KEY;
-const API_SECRET = process.env.API_SECRET;
-
-client.setApiKeySecret(API_KEY, API_SECRET);
+const client = require('./gateClient');
 
 //Fetch all futures contracts
 function fetchFuturesContracts() {
