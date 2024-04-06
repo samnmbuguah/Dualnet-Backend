c59f354bdfe18bba96513a49aef741d8a3ec68a5
Samuel <samnmbuguah@gmail.com>
2024-04-06 17:21:27 +0300

    🗃️ Update Scans table to set percentageDifference to -1

--- config/Dbsync.js
+++ config/Dbsync.js
@@ -4,10 +4,10 @@ const MatchingPairs = require('../models/MatchingPairsModel.js');
 const UserPDFs = require('../models/UserPDFsModel.js');
 const Scans = require('../models/ScansModel.js');
 
-Scans.drop()
+Scans.update({ percentageDifference: -1 }, { where: {} })
     .then(() => {
-        console.log('Scans table has been dropped');
+        console.log('percentageDifference in Scans table has been updated to -1');
         return db.sync();
     })
-    .then(() => console.log('Tables have been created'))
+    .then(() => console.log('Tables have been synced'))
     .catch(error => console.log('Error occurred:', error));
