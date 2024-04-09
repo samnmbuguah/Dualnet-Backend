faf6964c21c6236c871bf862c7238056f16973b4
Samuel <samnmbuguah@gmail.com>
2024-04-09 12:15:37 +0300

    🗃️  Add Bots model to models/BotsModel.js

---
+++ models/BotsModel.js
@@ -0,0 +1,70 @@
+const { Sequelize } = require("sequelize");
+const db = require("../config/Database.js");
+const MatchingPairs = require("./MatchingPairsModel.js");
+const Users = require("./UserModel.js");
+
+const Bots = db.define('Bots', {
+    userId: {
+        type: Sequelize.INTEGER
+    },
+    matchingPairId: {
+        type: Sequelize.STRING,
+        references: {
+            model: MatchingPairs,
+            key: 'id'
+        }
+    },
+    size: {
+        type: Sequelize.FLOAT
+    },
+    unrealisedPnl: {
+        type: Sequelize.FLOAT
+    },
+    realisedPnl: {
+        type: Sequelize.FLOAT
+    },
+    status: {
+        type: Sequelize.STRING
+    },
+    entryPrice: {
+        type: Sequelize.FLOAT
+    },
+    exitPrice: {
+        type: Sequelize.FLOAT
+    },
+    timestamp: {
+        type: Sequelize.DATE
+    },
+    leverage: {
+        type: Sequelize.FLOAT
+    },
+    tradeType: {
+        type: Sequelize.STRING
+    },
+    tradeId: {
+        type: Sequelize.STRING
+    },
+    orderId: {
+        type: Sequelize.STRING
+    },
+    currentPrice: {
+        type: Sequelize.FLOAT
+    },
+    pNL: {
+        type: Sequelize.FLOAT
+    },
+    cumulativePNL: {
+        type: Sequelize.FLOAT
+    }
+});
+
+// Define the relationship
+Bots.belongsTo(MatchingPairs, { foreignKey: 'matchingPairId' });
+MatchingPairs.hasMany(Bots, { foreignKey: 'id' });
+
+
+// Define the relationship with Users
+Bots.belongsTo(Users, { foreignKey: 'userId' });
+Users.hasMany(Bots, { foreignKey: 'id' });
+
+module.exports = Bots;
