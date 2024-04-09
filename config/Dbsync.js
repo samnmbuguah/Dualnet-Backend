c9c674abdd40fb835f9be28932e0c7fe183de82d
Samuel <samnmbuguah@gmail.com>
2024-04-09 15:43:58 +0300

    🗃️ ✨ Add database synchronization for Bots

--- config/Dbsync.js
+++ config/Dbsync.js
@@ -8,7 +8,19 @@ const Bots = require('../models/BotsModel.js');
 Scans.update({ percentageDifference: -1 }, { where: {} })
     .then(() => {
         console.log('percentageDifference in Scans table has been updated to -1');
-        return db.sync();
+        return Users.sync();
     })
-    .then(() => console.log('Tables have been synced'))
+    .then(() => {
+        console.log('Users table has been synced');
+        return MatchingPairs.sync({force: true});
+    })
+    .then(() => {
+        console.log('MatchingPairs table has been synced');
+        return UserPDFs.sync({force: true});
+    })
+    .then(() => {
+        console.log('Scans table has been synced');
+        return Bots.sync({force: true});
+    })
+    .then(() => console.log('Bots table has been synced'))
     .catch(error => console.log('Error occurred:', error));
