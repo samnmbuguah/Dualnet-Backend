cbe2aec4942878b4c99b1e6774f09ffaa7a9790d
Samuel <samnmbuguah@gmail.com>
2024-04-10 01:54:14 +0300

    🗃️ Add associations for Users and Bots models in associations.js

--- models/associations.js
+++ models/associations.js
@@ -1,6 +1,10 @@
 const MatchingPairs = require('./MatchingPairsModel');
 const Scans = require('./ScansModel');
+const Users = require("./UserModel.js");
+const Bots = require("./BotsModel.js");
 
 // Define the relationships
 MatchingPairs.hasOne(Scans, { foreignKey: 'matchingPairId' });
 Scans.belongsTo(MatchingPairs, { foreignKey: 'matchingPairId' });
+Users.hasMany(Bots, { foreignKey: 'userId' });
+Bots.belongsTo(Users, { foreignKey: 'userId' });
