46bc74274a8a4eb5ed4770db85976cd5e9e3e356
Samuel <samnmbuguah@gmail.com>
2024-04-09 15:48:18 +0300

    🗃️ ✨ Add listSpotOrderBook function to services

--- services/listSpotOrderBook.js
+++ services/listSpotOrderBook.js
@@ -3,12 +3,11 @@ const client = require('./gateClient.js');
 
 const api = new GateApi.SpotApi(client);
 
-function listOrderBook(currencyPair, opts) {
+async function listOrderBook(currencyPair, opts) {
     return api.listOrderBook(currencyPair, opts)
         .then(value => {
             const firstAsk = value.body.asks[0];
-            console.log('API called successfully');
-            console.log('First ask: ', firstAsk);
+            // console.log('First ask: ', firstAsk);
             return firstAsk;
         })
         .catch(error => {
@@ -20,9 +19,14 @@ function listOrderBook(currencyPair, opts) {
 module.exports = listOrderBook;
 
 
-// const currencyPair = "BTC_USDT"; // string | Spot currency pair
-// const opts = {
-//   'limit': 1, // number | Maximum number of order depth data in asks or bids
-// };
+// async function main() {
+//     const currencyPair = "BTC_USDT"; // string | Spot currency pair
+//     const opts = {
+//         'limit': 1, // number | Maximum number of order depth data in asks or bids
+//     };
 
-// listOrderBook(currencyPair, opts);
+//     const spotPrice = await listOrderBook(currencyPair, opts);
+//     console.log('Spot price: ', spotPrice[0]);
+// }
+
+// main().catch(console.error);
