2f854eae789d94b89baecb10263e63a696d0b105
Samuel <samnmbuguah@gmail.com>
2024-04-11 23:21:11 +0300

    🐛 Fix subClientId undefined error in fetchSpotBalance.js

--- services/fetchSpotBalance.js
+++ services/fetchSpotBalance.js
@@ -1,21 +1,31 @@
 const GateApi = require('gate-api');
-const client = require('./gateClient');
+const client = new GateApi.ApiClient();
+const getApiCredentials = require('./getApiCredentials');
 
 // Fetch spot account balance for a specific pair
-function fetchSpotBalance(pair) {
+async function fetchSpotBalance(pair, userId) { // Add the async keyword here
     console.log(`Fetching spot account balance for ${pair}...`);
-    const spotApi = new GateApi.SpotApi(client);
 
-    return spotApi.listSpotAccounts()
-        .then(response => {
-            const baseCurrency = pair.split('_')[0]; // Extract base currency from pair
-            const baseCurrencyBalance = response.body.find(account => account.currency === baseCurrency);
-            // console.log(`Spot ${baseCurrency} balance: `, baseCurrencyBalance.available);
-            // console.log('Spot account balance fetched successfully', baseCurrencyBalance, baseCurrency );
-            // console.log(response.body);
-            return baseCurrencyBalance;
-        })
-        .catch(error => console.error(error));
+    try {
+        const credentials = await getApiCredentials(userId);
+        if (!credentials) {
+            throw new Error('Could not fetch API credentials. Aborting trade.');
+        }
+    
+        client.setApiKeySecret(credentials.apiKey, credentials.apiSecret); 
+
+        const spotApi = new GateApi.SpotApi(client);
+
+        return spotApi.listSpotAccounts()
+            .then(response => {
+                const baseCurrency = pair.split('_')[0]; // Extract base currency from pair
+                const baseCurrencyBalance = response.body.find(account => account.currency === baseCurrency);
+                return baseCurrencyBalance;
+            })
+            .catch(error => console.error(error));
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 module.exports = fetchSpotBalance;
