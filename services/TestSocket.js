94224949f78d2a98fdff614800dc545e74d6d584
Samuel <samnmbuguah@gmail.com>
2024-04-12 14:14:08 +0300

    ✅  Update TestSocket.js to join a specific room and listen for botData event

--- services/TestSocket.js
+++ services/TestSocket.js
@@ -5,17 +5,22 @@ const socket = io.connect('http://localhost:3042');
 
 socket.on('connect', () => {
     console.log('Connected to the server');
-
-    // Emit the 'updateScans' event every 30 seconds
-    setInterval(() => {
-        console.log('Sending updateScans event to the server');
-        socket.emit('updateScans');
-    }, 10000); // 30000 milliseconds = 30 seconds
+    socket.emit('join', 18);
+    // // Emit the 'updateScans' event every 30 seconds
+    // setInterval(() => {
+    //     console.log('Sending updateScans event to the server');
+    //     socket.emit('updateScans');
+    // }, 10000); // 30000 milliseconds = 30 seconds
 });
 
 // Listen for the 'topScans' event to receive the top scans from the server
 socket.on('topScans', (data) => {
     console.log('Received topScans from the server:', data);
+});
+
+// Listen for the 'botData' event to receive the bot data from the server
+socket.on('botData', (data) => {
+    console.log('Received botData from the server:', data);
 });
 
 socket.on('disconnect', (reason) => {
