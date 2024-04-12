071e54670cef05eafc27a832e0aec2cc7d8f8576
Samuel <samnmbuguah@gmail.com>
2024-04-12 14:12:07 +0300

    🐛 Fix fetchPosition function in checkTrades.js to include userId parameter

--- services/checkTrades.js
+++ services/checkTrades.js
@@ -24,7 +24,7 @@ async function checkTrades() {
       // Calculate balance in USDT
       const balanceInUsdt = balance.available * bot.currentPrice;
       // Fetch the futures position for the pair
-      const position = await fetchPosition(bot.settle, bot.matchingPairId);
+      const position = await fetchPosition(bot.settle, bot.matchingPairId, bot.userId);
       console.log('Balance in USDT:', balanceInUsdt);
       // If the size of the futures position is less than 0 and balanceInUsdt is less than 1
       // or if balanceInUsdt is greater than 1 and size is 0
