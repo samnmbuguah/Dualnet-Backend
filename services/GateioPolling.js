acfe09e275ebbf3ad11483a7637910959c157c70
Samuel <samnmbuguah@gmail.com>
2024-04-15 10:29:43 +0300

    🐛 Refactor GateioPolling.js to use getCurrentSpotPrice and getContractDetails functions

--- services/GateioPolling.js
+++ services/GateioPolling.js
@@ -1,14 +1,6 @@
-require('dotenv').config();
-const GateApi = require('gate-api');
-const apiKey = process.env.API_KEY;
-const apiSecret = process.env.API_SECRET;
-const client = new GateApi.ApiClient();
-client.setApiKeySecret(apiKey, apiSecret);
-const spotApi = new GateApi.SpotApi(client);
-const futuresApi = new GateApi.FuturesApi(client);
 const Scans = require('../models/ScansModel.js');
-const listFuturesOrderBook = require('./listFuturesOrderBook.js');
-const listOrderBook = require('./listSpotOrderBook.js');
+const getContractDetails = require('./getContractDetails.js');
+const getCurrentSpotPrice = require('./getCurrentSpotPrice');
 
 class PollPrices {
     constructor(tickers, settle, amountPrecisions) {
@@ -21,15 +13,15 @@ class PollPrices {
    async fetchAndUpdateScans() {
     const promises = this.tickers.map(async (ticker, index) => {
         try {
-            const spotResponse = await listOrderBook(ticker, { limit: 1 });
-            const futuresResponse = await listFuturesOrderBook(this.settle, ticker, { limit: 1 });
+            const spotResponse = await getCurrentSpotPrice(ticker);
+            const futuresResponse =  await getContractDetails(this.settle, ticker);
 
-            const spotPrice = parseFloat(spotResponse[0]);
-            const futuresPrice = parseFloat(futuresResponse.p);
+            const spotPrice = parseFloat(spotResponse.lowestAsk);
+            const futuresPrice = parseFloat(futuresResponse.indexPrice);
 
-            let valueDifference = futuresPrice - spotPrice;
-            valueDifference = parseFloat(valueDifference.toFixed(this.amountPrecisions[index]));
-            let percentageDifference = ((futuresPrice - spotPrice) / spotPrice) * 100;
+            
+            let valueDifference = (futuresPrice - spotPrice).toFixed(this.amountPrecisions[index] + 2);
+            let percentageDifference = (valueDifference / spotPrice) * 100;
             percentageDifference = parseFloat(percentageDifference.toFixed(4));
 
             await Scans.upsert({
