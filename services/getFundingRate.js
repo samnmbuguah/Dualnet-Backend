0e8e36421c8002ec9f3f842a9eabb35f1c2034c3
Samuel <samnmbuguah@gmail.com>
2024-04-17 11:37:30 +0300

    ⚡️ Fetch and update funding rates for all futures contracts every 10 mins

--- services/getFundingRate.js
+++ services/getFundingRate.js
@@ -2,34 +2,28 @@ const GateApi = require('gate-api');
 const client = new GateApi.ApiClient();
 const Scans = require('../models/ScansModel.js');
 const cron = require('node-cron');
-
-const api = new GateApi.FuturesApi(client);
-const settle = "usdt"; // 'btc' | 'usdt' | 'usd' | Settle currency
+const settle = "usdt";
 
 const updateFundingRate = async () => {
-    // Fetch all scans
-    const scans = await Scans.findAll();
-
-    // Update the fundingRate for each scan
-    for (const scan of scans) {
-        const contract = scan.matchingPairId; // Use the matchingPairId as the contract
-        const opts = {
-            'limit': 1 // number | Maximum number of records to be returned in a single list
-        };
-
-        try {
-            const value = await api.listFuturesFundingRateHistory(settle, contract, opts);
-            const fundingRate = parseFloat(value.body[0].r);
+    try {
+        console.log('Fetching futures contracts...');
+        const futuresApi = new GateApi.FuturesApi(client);
+        
+        const value = await futuresApi.listFuturesContracts(settle);
+        const contracts = value.body;
+        for (const contract of contracts) {
+            const fundingRate = parseFloat(contract.fundingRate);
             // Update the fundingRate field
-            await Scans.update({ fundingRate }, { where: { matchingPairId: contract } });
-            console.log(`Updated fundingRate for ${contract} to ${fundingRate}`);
-        } catch (error) {
-            console.error(`Failed to update fundingRate for ${contract}: ${error}`);
+            await Scans.update({ fundingRate }, { where: { matchingPairId: contract.name } });
+            console.log(`Updated fundingRate for ${contract.name} to ${fundingRate}`);
         }
+    } catch (error) {
+        console.error(`Failed to update funding rates: ${error}`);
     }
 };
 
 // Schedule a cron job to run at the top of every hour
 // cron.schedule('0 * * * *', updateFundingRate);
 
+// updateFundingRate()
 module.exports = updateFundingRate;
