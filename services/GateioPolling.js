567c15b1dbb61e817676418d304ea5e903a67651
Samuel <samnmbuguah@gmail.com>
2024-04-18 13:53:43 +0300

    ♻️ Refactor GateioPolling.js to handle add rate limiting

--- services/GateioPolling.js
+++ services/GateioPolling.js
@@ -1,46 +1,69 @@
-const Scans = require('../models/ScansModel.js');
-const getContractDetails = require('./getContractDetails.js');
-const getCurrentSpotPrice = require('./getCurrentSpotPrice');
+const Scans = require("../models/ScansModel.js");
+const getContractDetails = require("./getContractDetails.js");
+const getCurrentSpotPrice = require("./getCurrentSpotPrice");
+
+// Helper function to handle rate limitting
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 class PollPrices {
-    constructor(tickers, settle, amountPrecisions) {
-        this.amountPrecisions = amountPrecisions;
-        this.tickers = tickers;
-        this.settle = settle;
-        this.lastPrices = {};
+  constructor(tickers, settle, amountPrecisions) {
+    this.amountPrecisions = amountPrecisions;
+    this.tickers = tickers;
+    this.settle = settle;
+    this.lastPrices = {};
+  }
+
+  updateTickers(tickers, amountPrecisions) {
+    this.tickers = tickers;
+    this.amountPrecisions = amountPrecisions;
+  }
+
+  async fetchAndUpdateScans() {
+    const delayInMilliseconds = 10000 / 200; // 50 milliseconds
+
+    for (let index = 0; index < this.tickers.length; index++) {
+      const ticker = this.tickers[index];
+
+      const startTime = Date.now();
+
+      try {
+        const spotResponse = await getCurrentSpotPrice(ticker);
+        const futuresResponse = await getContractDetails(this.settle, ticker);
+
+        const spotPrice = parseFloat(spotResponse.last);
+        const futuresPrice = parseFloat(futuresResponse.lastPrice);
+
+        let valueDifference = (futuresPrice - spotPrice).toFixed(
+          this.amountPrecisions[index] + 2
+        );
+        let percentageDifference = (valueDifference / spotPrice) * 100;
+        percentageDifference = parseFloat(percentageDifference.toFixed(4));
+
+        await Scans.upsert({
+          matchingPairId: ticker,
+          futuresPrice: futuresPrice,
+          spotPrice: spotPrice,
+          valueDifference: valueDifference,
+          percentageDifference: percentageDifference,
+        });
+        console.log(`Updated scan for ticker ${ticker}`);
+
+      } catch (error) {
+        console.error(`Failed to update scan for ticker ${ticker}: ${error}`);
+      }
+
+      const endTime = Date.now();
+      const apiCallTime = endTime - startTime;
+
+      const actualDelay = Math.max(0, delayInMilliseconds - apiCallTime);
+
+      await delay(actualDelay);
     }
 
-   async fetchAndUpdateScans() {
-    const promises = this.tickers.map(async (ticker, index) => {
-        try {
-            const spotResponse = await getCurrentSpotPrice(ticker);
-            const futuresResponse =  await getContractDetails(this.settle, ticker);
-
-            const spotPrice = parseFloat(spotResponse.last);
-            const futuresPrice = parseFloat(futuresResponse.lastPrice);
-
-            
-            let valueDifference = (futuresPrice - spotPrice).toFixed(this.amountPrecisions[index] + 2);
-            let percentageDifference = (valueDifference / spotPrice) * 100;
-            percentageDifference = parseFloat(percentageDifference.toFixed(4));
-
-            await Scans.upsert({
-                matchingPairId: ticker,
-                futuresPrice: futuresPrice,
-                spotPrice: spotPrice,
-                valueDifference: valueDifference,
-                percentageDifference: percentageDifference
-            });
-            // console.log("Upserted ", ticker, " with futures price ", futuresPrice, " and spot price ", spotPrice, " and percentage difference ", percentageDifference);
-            // console.log(`Scan for ticker ${ticker} updated successfully`);
-        } catch (error) {
-            console.error(`Failed to update scan for ticker ${ticker}: ${error}`);
-        }
-    });
-
-    await Promise.allSettled(promises);
-
-    return 'Top scans updated in the database';
-}
+    console.log("All scans updated successfully");
+    return true;
+  }
 }
 module.exports = PollPrices;
