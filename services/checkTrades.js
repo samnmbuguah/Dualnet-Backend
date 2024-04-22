a5ca9ff353bd74aa3e4136cc01369e8f7f1c0752
Samuel <samnmbuguah@gmail.com>
2024-04-22 14:03:42 +0300

    ✨ Use different functions to handle closing short positions and selling spot positions

--- services/checkTrades.js
+++ services/checkTrades.js
@@ -2,8 +2,7 @@ const fetchSpotBalance = require('./fetchSpotBalance');
 const fetchPosition = require('./getPosition');
 const Bots = require('../models/BotsModel.js');
 const cron = require('node-cron');
-const sellSpotAndLongFutures = require('./closeTrades');
-
+const { closeShort, sellSpotPosition } = require('./checkCloseTrades');
 const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
 
@@ -11,32 +10,37 @@ const Op = Sequelize.Op;
 async function checkTrades() {
   try {
     console.log('checkTrades function called');
-    // Fetch all bots where isClose is false
     const bots = await Bots.findAll({ 
-      attributes: ["userId", "matchingPairId", "currentPrice", "settle", "isClose"],
+      attributes: ["userId", "matchingPairId", "currentPrice", "settle", "isClose", "positionId", "quantoMultiplier"],
       where: { isClose: false } 
     });
     console.log('Bots fetched successfully', bots.length);
-    // console.log(bots);
+
     for (const bot of bots) {
-      // Fetch the spot balance for the pair
       const balance = await fetchSpotBalance(bot.matchingPairId, bot.userId);
-      // Calculate balance in USDT
       const balanceInUsdt = balance.available * bot.currentPrice;
-      // Fetch the futures position for the pair
       const position = await fetchPosition(bot.settle, bot.matchingPairId, bot.userId);
-      // console.log('Balance in USDT:', balanceInUsdt);
-      // If the size of the futures position is less than 0 and balanceInUsdt is less than 1
-      // or if balanceInUsdt is greater than 1 and size is 0
-      if ((position.size < 0 && balanceInUsdt < 1) || (balanceInUsdt > 1 && position.size === 0)) {
-        console.log('Exiting spot and futures');
+      const positionSize = position ? position.size : 0; // Handle undefined position
+
+      if (positionSize < 0 && balanceInUsdt < 1) {
+        console.log('Closing short');
         try {
-          await sellSpotAndLongFutures(bot.matchingPairId, bot.userId, position.size, balance, bot.positionId);
-          console.log('Spot and futures exited successfully');
+          await closeShort(bot.matchingPairId, bot.userId, positionSize, bot.positionId, bot.quantoMultiplier);
+          console.log('Short closed successfully');
+        } catch (error) {
+          console.error('Error during closing short:', error);
+        }
+      }
+
+      if (balanceInUsdt > 1 && positionSize == 0) {
+        console.log('Selling spot');
+        try {
+          await sellSpotPosition(bot.matchingPairId, bot.userId, balance, bot.positionId);
+          console.log('Spot sold successfully');
           await Bots.update({ isClose: true }, { where: { matchingPairId: bot.matchingPairId, userId: bot.userId } });
           console.log('Bots updated successfully');
         } catch (error) {
-          console.error('Error during trading:', error);
+          console.error('Error during selling spot:', error);
         }
       }
     }
@@ -46,7 +50,6 @@ async function checkTrades() {
 }
 
 module.exports = checkTrades;
-
 
 // // Schedule the task to run every minute
 // cron.schedule('* * * * *', checkTrades);
