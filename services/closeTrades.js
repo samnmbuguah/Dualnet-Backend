d5620cd36c7a26bae151d653936e06fc003d5e55
Samuel <samnmbuguah@gmail.com>
2024-04-22 14:05:58 +0300

    ♻️ Refactor to improve readability and error handling

--- services/closeTrades.js
+++ services/closeTrades.js
@@ -1,70 +1,94 @@
-const GateApi = require('gate-api');
+const GateApi = require("gate-api");
 const client = new GateApi.ApiClient();
-const Bots = require('../models/BotsModel.js');
-const fetchSpotBalance = require('./fetchSpotBalance');
-const getApiCredentials = require('./getApiCredentials');
+const Bots = require("../models/BotsModel.js");
+const fetchSpotBalance = require("./fetchSpotBalance");
+const getApiCredentials = require("./getApiCredentials");
 
-async function sellSpotAndLongFutures(pair, subClientId, futuresSize = 0, spotSize, positionId) {
-    try {
-        const credentials = await getApiCredentials(subClientId);
-        if (!credentials) {
-            throw new Error('Could not fetch API credentials. Aborting trade.');
-        }
-        
-        client.setApiKeySecret(credentials.apiKey, credentials.apiSecret);
-        
-        const spotApi = new GateApi.SpotApi(client);
-        const futuresApi = new GateApi.FuturesApi(client);
-        
-        // Create a spot order to sell the entire spot balance
-        const order = new GateApi.Order();
-        order.account = "spot";
-        order.type = "market";
-        order.currencyPair = pair; 
-        order.amount = spotSize; 
-        order.side = "sell";
-        order.timeInForce = 'ioc';
-        
-        // Fetch the spot balance for the base currency of the pair
-        const baseCurrency = pair.split('_')[0]; // Extract base currency from pair
-        const spotBalance = await fetchSpotBalance(baseCurrency, subClientId);
-        // Convert spotBalance.available and spotSize to numbers for comparison
-        const availableSpotBalance = Number(spotBalance.available);
-        const desiredSpotSize = Number(spotSize);
 
-        // If available spot balance is less than the desired spot size, use the available spot balance
-        if (availableSpotBalance < desiredSpotSize) {
-            order.amount = spotBalance.available;
-        }
 
-        // Try to create the spot order
-        spotApi.createOrder(order)
-        .then(response => console.log('Spot sell order response', response.body))
-        .catch(error => console.error(error.response));
-        
-        // Create a futures order to close the entire futures position
+async function sellSpotAndLongFutures(
+  pair,
+  subClientId,
+  futuresSize = 0,
+  spotSize,
+  positionId,
+  multiplier
+) {
+  try {
+    const credentials = await getApiCredentials(subClientId);
+    if (!credentials) {
+      throw new Error("Could not fetch API credentials. Aborting trade.");
+    }
+
+    client.setApiKeySecret(credentials.apiKey, credentials.apiSecret);
+
+    const spotApi = new GateApi.SpotApi(client);
+    const futuresApi = new GateApi.FuturesApi(client);
+    const order = new GateApi.Order();
+    order.account = "spot";
+    order.type = "market";
+    order.currencyPair = pair;
+    order.amount = spotSize;
+    order.side = "sell";
+    order.timeInForce = "ioc";
+
+    const baseCurrency = pair.split("_")[0];
+    const spotBalance = await fetchSpotBalance(baseCurrency, subClientId);
+    const availableSpotBalance = Number(spotBalance.available);
+
+    if (availableSpotBalance <= spotSize) {
+      order.amount = availableSpotBalance;
+    }
+
+    return spotApi
+      .createOrder(order)
+      .then(async (response) => {
+        console.log("Spot sell order response", response.body);
+
+        const amount = futuresSize / multiplier;
         const futuresOrder = new GateApi.FuturesOrder();
         futuresOrder.contract = pair;
-        futuresOrder.settle = 'usdt';
-        futuresOrder.size = -futuresSize;
-        futuresOrder.price = '0'; // Market order  
-        futuresOrder.tif = 'ioc'; 
+        futuresOrder.settle = "usdt";
+        futuresOrder.size = amount;
+        futuresOrder.price = "0";
+        futuresOrder.tif = "ioc";
 
-        futuresApi.createFuturesOrder('usdt', futuresOrder)
-            .then(response => console.log('Futures close order response', response.body))
-            .catch(error => console.error(error.response));
-        
-       // Update the bots table
-        await Bots.update({ isClose: true }, {
+        futuresApi
+          .createFuturesOrder("usdt", futuresOrder)
+          .then((response) =>
+            console.log("Futures close order response", response.body)
+          )
+          .catch((error) => console.error(error.response));
+
+        await Bots.update(
+          { isClose: true },
+          {
             where: {
-                positionId: positionId
-            }
-        }); 
+              positionId: positionId,
+            },
+          }
+        );
         return true;
-    } catch (error) {
-        console.error('Error during trading:', error);
+      })
+      .catch((error) => {
+        console.error(error.response);
         return false;
-    }
+      });
+  } catch (error) {
+    console.error("Error during trading:", error);
+    return false;
+  }
 }
 
 module.exports = sellSpotAndLongFutures;
+
+// const tradeData = {"pair":"POGAI_USDT","subClientId":3,"futuresSize":40000,"spotSize":39986.13154930825,"positionId":"9d86b592-1bad-43e5-93e9-a8b033cc9038","multiplier":10000}
+
+// sellSpotAndLongFutures(
+//   tradeData.pair,
+//   tradeData.subClientId,
+//   tradeData.futuresSize,
+//   tradeData.spotSize,
+//   tradeData.positionId
+//   tradeData.multiplier
+// );
