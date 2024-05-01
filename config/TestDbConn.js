d0e7646b2fd67ed35e6514c060b3b2bca6b1c9a4
Samuel <samnmbuguah@gmail.com>
2024-05-01 19:54:29 +0300

    🗃️ Code for backing up the database

--- config/TestDbConn.js
+++ config/TestDbConn.js
@@ -3,3 +3,5 @@ const db = require('./Database.js');
 db.authenticate()
     .then(() => console.log('Connection has been established successfully.'))
     .catch(error => console.error('Unable to connect to the database:', error));
+
+    // PGPASSWORD=VFwQxOHkqOHYTsGCGjifjqJGOGlCWPZU pg_dump -h viaduct.proxy.rlwy.net -U postgres -p 16646 -d railway > backup.sql
