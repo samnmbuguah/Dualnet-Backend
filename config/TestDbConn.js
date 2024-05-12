84772a5ca39db2537b14c81952bd9d1e86d69471
Samuel <samnmbuguah@gmail.com>
2024-05-12 09:35:59 +0300

    🔥 Delete sensitive info

--- config/TestDbConn.js
+++ config/TestDbConn.js
@@ -4,4 +4,3 @@ db.authenticate()
     .then(() => console.log('Connection has been established successfully.'))
     .catch(error => console.error('Unable to connect to the database:', error));
 
-    // PGPASSWORD=VFwQxOHkqOHYTsGCGjifjqJGOGlCWPZU pg_dump -h viaduct.proxy.rlwy.net -U postgres -p 16646 -d railway > backup.sql
