7a161e8839d3efab92d1794fc75b3d5a64187596
Samuel <samnmbuguah@gmail.com>
2024-05-29 18:09:40 +0300

    ⚡️ refactor setLeverage function for reusability

--- services/setLeverage.js
+++ services/setLeverage.js
@@ -2,30 +2,28 @@ const GateApi = require("gate-api");
 const client = new GateApi.ApiClient();
 const getApiCredentials = require("./getApiCredentials");
 
-async function setLeverage(req, res) {
-  try {
-    const { settle, contract, leverage = "1", subClientId } = req.body;
-
+async function setLeverage(settle, contract, leverage = 1, subClientId) {
     const credentials = await getApiCredentials(subClientId);
     if (!credentials) {
-      throw new Error("Could not fetch API credentials. Aborting trade.");
+        throw new Error("Could not fetch API credentials. Aborting trade.");
     }
 
     client.setApiKeySecret(credentials.apiKey, credentials.apiSecret);
-
     const futuresApi = new GateApi.FuturesApi(client);
-
-    const opts = {
-      crossLeverageLimit: leverage, // string | Cross margin leverage(valid only when `leverage` is 0)
+    const options = {
+      crossLeverageLimit: leverage, // string | Cross margin leverage(when `leverage` is 0)
     };
 
-    const response = await futuresApi.updatePositionLeverage(settle, contract, 0, opts);
-    console.log("Leverage changed successfully", response.body);
-    res.json(response.body);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: error.message });
-  }
+    return futuresApi
+        .updatePositionLeverage(settle, contract,0 , options)
+        .then((response) => {
+            console.log("Leverage set", response.body);
+            return response.body;
+        })
+        .catch((error) => {
+            console.error(error.response);
+            throw error;
+        });
 }
 
 module.exports = setLeverage;
